8b9e83e25ee82bb03f88cfcf7b7ded2d11e3cad0
bakmie234 <bakmie234@gmail.com>
2015-09-20 14:23:39 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080,
+        files: ["*/**/*.*"]
+    });
+});
