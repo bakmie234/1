70559a3b0d85adfe9debdcbe9681c00bbdbbf4c2
bakmie234 <bakmie234@gmail.com>
2015-09-20 23:56:58 +0700

    todo list

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,18 @@ gulp.task('default', function() {
         files: ["*/**/*.*"]
     });
 });
+
+var inject = require('gulp-inject');
+gulp.task('inject', function () {
+    var target = gulp.src('./todoList.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources =
+        gulp.src([
+            './todoList/todoList.js',
+            './todoList/**/*.js'
+        ],
+        {read: false});
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./'));
+});
---
+++ todoList/todoList.js
@@ -0,0 +1,6 @@
+/**
+ * Created by bagus on 9/20/2015.
+ */
+(function () {
+    angular.module("todoList",[]);
+}());
